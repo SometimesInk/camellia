2d3a49c60ab5c680fba4252a0ebe636c4e19e7e5
Ink <pandainvasionca@gmail.com>
2026-06-24 01:27:03 -0400

    fix: fixed substringing heap overflow
    Fixed how substringing allocated overflowed the allocated space by
    adding the null terminator by increasing the allocated memory by one.
    Fixed how the substringing unit test was completely flawed and could not
    test anything. It now tests edge cases more rigorously.
    Added flags to find these errors in CMake testing setup.

--- src/camellia/type/string.c
+++ src/camellia/type/string.c
@@ -8,8 +8,9 @@
 // TODO: TEST
 cam_out_t cam_type_substring(cam_str_t *sub, const cam_cptr_t str,
                              const cam_size_t start, const cam_size_t end) {
+  CAM_ERR_CHECK(end >= start, CAM_ERR_INV_ARG);
   sub->len = end - start + 1;
-  sub->str = (cam_cptr_t)malloc(sub->len);
+  sub->str = (cam_cptr_t)malloc(sub->len + 1);
   CAM_ERR_CHECK(sub->str, CAM_ERR_MEM_ALLOC);
   (void)memcpy(sub->str, str + start, sub->len);
   sub->str[sub->len] = '\0';
--- tests/test_string.c
+++ tests/test_string.c
@@ -9,6 +9,9 @@
 cam_str_t substring1;
 cam_str_t substring2;
 cam_str_t substring3;
+cam_str_t substring4;
+cam_str_t substring5;
+cam_str_t substring6;
 
 #undef CAM_TEST_RETURN
 #define CAM_TEST_RETURN(v)                                                     \
@@ -16,6 +19,9 @@ cam_str_t substring3;
     free(substring1.str);                                                      \
     free(substring2.str);                                                      \
     free(substring3.str);                                                      \
+    free(substring4.str);                                                      \
+    free(substring5.str);                                                      \
+    free(substring6.str);                                                      \
     printf("Last err: %u.", cam_err_get());                                    \
     return v;                                                                  \
   } while (0);
@@ -30,17 +36,35 @@ cam_test_result_t main(void) {
   CAM_TEST_ASSERT_SUCCESS(cam_type_substring(&substring1, string, 3, 6));
   CAM_TEST_ASSERT_PREV_ERR_NOT(CAM_ERR_MEM_ALLOC);
   printf("Substring1: %s\n", substring1.str);
-  CAM_TEST_ASSERT(strcmp(string, "ethi"));
+  CAM_TEST_ASSERT(substring1.len == 4);
+  CAM_TEST_ASSERT(strcmp(substring1.str, "ethi") == 0);
 
   CAM_TEST_ASSERT_SUCCESS(cam_type_substring(&substring2, string, 0, 6));
   CAM_TEST_ASSERT_PREV_ERR_NOT(CAM_ERR_MEM_ALLOC);
   printf("Substring2: %s\n", substring2.str);
-  CAM_TEST_ASSERT(strcmp(string, "Somethi"));
+  CAM_TEST_ASSERT(substring2.len == 7);
+  CAM_TEST_ASSERT(strcmp(substring2.str, "Somethi") == 0);
 
-  CAM_TEST_ASSERT_SUCCESS(cam_type_substring(&substring3, string, 3, 9));
+  CAM_TEST_ASSERT_SUCCESS(cam_type_substring(&substring3, string, 9, 9));
   CAM_TEST_ASSERT_PREV_ERR_NOT(CAM_ERR_MEM_ALLOC);
   printf("Substring3: %s\n", substring3.str);
-  CAM_TEST_ASSERT(strcmp(string, "ething."));
+  CAM_TEST_ASSERT(substring3.len == 1);
+  CAM_TEST_ASSERT(strcmp(substring3.str, ".") == 0);
+
+  CAM_TEST_ASSERT_SUCCESS(cam_type_substring(&substring4, string, 0, 0));
+  CAM_TEST_ASSERT_PREV_ERR_NOT(CAM_ERR_MEM_ALLOC);
+  printf("Substring4: %s\n", substring4.str);
+  CAM_TEST_ASSERT(substring4.len == 1);
+  CAM_TEST_ASSERT(strcmp(substring4.str, "S") == 0);
+
+  CAM_TEST_ASSERT_SUCCESS(cam_type_substring(&substring5, string, 0, 9));
+  CAM_TEST_ASSERT_PREV_ERR_NOT(CAM_ERR_MEM_ALLOC);
+  printf("Substring5: %s\n", substring5.str);
+  CAM_TEST_ASSERT(substring5.len == 10);
+  CAM_TEST_ASSERT(strcmp(substring5.str, "Something.") == 0);
+
+  CAM_TEST_ASSERT_FAILURE(cam_type_substring(&substring6, string, 6, 3));
+  CAM_TEST_ASSERT_PREV_ERR(CAM_ERR_INV_ARG);
 
   // Match
   CAM_TEST_ASSERT(cam_type_str_match(string, "Something!", 5) == CAM_SUCCESS);
